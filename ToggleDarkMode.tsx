863617c09eaf92d18e08e06bf150ea5bb2602b18
Putra PPJ <137275265+ppjslawi@users.noreply.github.com>
2025-03-24 10:48:57 +0700

    update

---
+++ ToggleDarkMode.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import styles from "./ToggleDarkMode.module.css";
+
+const ToggleDarkMode = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setIsDarkMode((prev) => !prev);
+        document.body.classList.toggle("dark-mode");
+    };
+
+    return (
+        <div 
+            className={`${styles.toggle} ${isDarkMode ? styles.dark : ""}`} 
+            onClick={toggleDarkMode}
+        >
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g className={styles['sun-beams']} stroke="currentColor">
+                    <line x1="12" y1="1" x2="12" y2="3"></line>
+                    <line x1="12" y1="21" x2="12" y2="23"></line>
+                    <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                    <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+                    <line x1="1" y1="12" x2="3" y2="12"></line>
+                    <line x1="21" y1="12" x2="23" y2="12"></line>
+                    <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                    <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+                </g>
+            </svg>
+            <div className={styles.circle}></div>
+            <div className={styles.moon}></div>
+        </div>
+    );
+};
+
+export default ToggleDarkMode;
